15ab5efb5bbeb39b3c6be7f57affd901b689baf8
Jonathan Berlemont <berlemontjonathan@gmail.com>
2019-01-29 15:26:54 +0100

    correction du php

--- src/php/music/dropdown_get_genres.php
+++ src/php/music/dropdown_get_genres.php
@@ -3,12 +3,10 @@
                             FROM musique
                             ');
     while ($donnees = $requete->fetch()) {
-        echo '
-        <label class="dropdown-item">
-        <input type="checkbox" name="genres[]" value="'.$donnees['genre'].' "';
+        echo '<label class="dropdown-item">';
+        echo '<input type="checkbox" name="genres[]" value="'.$donnees['genre'].'"';
         echo ((isset($_GET['genres']) && in_array($donnees['genre'], $_GET['genres']))) ? 'checked' : null ; 
         echo '>'.$donnees['genre'];
-        echo '</label>'
-        ;
+        echo '</label>';
     }
 ?>
